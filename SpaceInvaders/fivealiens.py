698a4823f380fe36baf8c256b884b5b3096893e6
Joe Haig <joe.haig@bbc.co.uk>
2014-05-22 14:53:18 -0700

    A line of five aliens

---
+++ SpaceInvaders/fivealiens.py
@@ -0,0 +1,23 @@
+...
+
+ship = GamePiece(150, 260, ship_image)
+aliens = []                                         # Changed
+for i in range(5):                                  # New
+    alien = GamePiece(30 + i*50, 30, alien_image)   # New
+    # Keep aliens in line when they bounce          # New
+    alien.min_x = 20 + i*50                         # New
+    alien.max_x = 140 + i*50                        # New
+    # Make the alien move by itself                 # Changed
+    alien.move_left = True                          # Changed
+    # Make the alien bounce when it hits the edge   # Changed
+    alien.bounce = True                             # Changed
+    alien.speed = 0.3                               # Changed
+    # Set the height and width of the alien         # Changed
+    alien.width = 47                                # Changed
+    alien.height = 22                               # Changed
+    aliens.append(alien)                            # New
+bullet = None
+
+run = True
+
+...
---
+++ SpaceInvaders/moveallaliens.py
@@ -0,0 +1,27 @@
+...
+
+    # Move and draw the ship
+    ship.move()
+    ship.draw()
+
+    # Move and draw the aliens                              # Changed
+    for alien in aliens:                                    # Changed
+        alien.move()                                        # Changed
+        alien.draw()                                        # Changed
+
+    # Move and draw the bullet
+    if bullet != None:
+        bullet.move()
+        if bullet.y > bullet.min_y:
+            bullet.draw()
+            for alien in aliens:                            # Changed
+                if alien.detect_hit(bullet):                # Changed
+                    aliens.remove(alien)                    # Changed
+                    bullet = None                           # Changed
+                    break                                   # Changed
+        else:
+            bullet = None
+
+    # Refresh the screen
+    pygame.display.update()
+    clock.tick(game_speed)
