e65226f8bf9f44c4b9a4dcdfbb8789b9c6b5745c
Joe Haig <joe.haig@bbc.co.uk>
2014-05-24 14:46:35 -0700

    Add the score

---
+++ SpaceInvaders/snippets/scorehit.py
@@ -0,0 +1,16 @@
+    # Move and draw the bullet
+    if bullet != None:
+        bullet.move()
+        if bullet.y > bullet.min_y:
+            bullet.draw()
+            for alien in aliens:
+                if alien.detect_hit(bullet):
+                    aliens.remove(alien)
+                    bullet = None
+                    score = score + 10                        # New
+                    break
+        else:
+            bullet = None
+
+    score_text = font.render("Score: " + str(score), 1, BLUE) # New
+    screen.blit(score_label, (10, 10))                        # New
---
+++ SpaceInvaders/snippets/scorevariable.py
@@ -0,0 +1,16 @@
+import pygame
+
+# Colours
+WHITE = (255,255,255)
+BLUE = (0, 0, 255)                            # New
+
+...
+
+run = True
+score = 0                                     # New
+font = pygame.font.SysFont("monospace", 12)   # New
+
+while run:
+    screen.fill(WHITE)
+
+...
