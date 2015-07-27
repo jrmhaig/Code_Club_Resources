ed45a9ae2f17c37b5e1d8cfc589198c316fdbd1a
Joseph Haig <josephhaig@gmail.com>
2015-07-27 02:31:05 -0700

    Add one alien moving across the screen

--- SpaceInvaders/space_invaders.py
+++ SpaceInvaders/space_invaders.py
@@ -7,10 +7,18 @@ ship.x_min = (5 * WIDTH) / 100
 ship.x_max = (95 * WIDTH) / 100
 
 class Alien(Actor):
-    def __init__(self, image):
-        super(image)
+    def __init__(self, image, pos, x_min, x_max, anchor=('center', 'center')):
+        super(Alien, self).__init__(image, pos, anchor)
+        self.x_min = x_min
+        self.x_max = x_max
+        self.speed = 1
 
-alien1 = Actor('alien1')
+    def move(self):
+        self.x += self.speed
+        if self.x >= self.x_max or self.x <= self.x_min:
+            self.speed = -self.speed
+
+alien1 = Alien('alien1', (100, 100), 100, 500)
 
 def draw():
     screen.clear()
@@ -22,3 +30,4 @@ def update():
         ship.x -= ship.speed
     if keyboard.RIGHT and ship.x < ship.x_max:
         ship.x += ship.speed
+    alien1.move()
