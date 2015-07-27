ffae47e1faed39b8e0c94e2fd4776821b11569c3
Joseph Haig <josephhaig@gmail.com>
2015-07-27 09:13:23 -0700

    Fire bullets

--- SpaceInvaders/space_invaders.py
+++ SpaceInvaders/space_invaders.py
@@ -1,14 +1,29 @@
 WIDTH = 800
 HEIGHT = 600
+ALIEN_DATA = [
+    { 'image': 'alien1', 'bullet': 'alien1-bullet' },
+    { 'image': 'alien2', 'bullet': 'alien2-bullet' },
+    { 'image': 'alien3', 'bullet': 'alien3-bullet' },
+    { 'image': 'alien4', 'bullet': 'alien4-bullet' }]
+BULLET_SPEED = 3
+MAX_BULLETS = 5
 
 ship = Actor('ship', (WIDTH / 2, (95 * HEIGHT) / 100 ))
 ship.speed = 5
 ship.x_min = (5 * WIDTH) / 100
 ship.x_max = (95 * WIDTH) / 100
 
+class Bullet(Actor):
+    def __init__(self, speed, *args, **kwargs):
+        super(Bullet, self).__init__(*args, **kwargs)
+        self.speed = speed
+
+    def move(self):
+        self.y += self.speed
+
 class Alien(Actor):
-    def __init__(self, image, pos, x_min, x_max, anchor=('center', 'center')):
-        super(Alien, self).__init__(image, pos, anchor)
+    def __init__(self, alien_type, pos, x_min, x_max, anchor=('center', 'center')):
+        super(Alien, self).__init__(ALIEN_DATA[alien_type]['image'], pos, anchor)
         self.x_min = x_min
         self.x_max = x_max
         self.h_speed = 1
@@ -32,18 +47,31 @@ for n in range(ALIEN_ROWS):
   for i in range(ALIEN_ROW_LENGTH):
       aliens.append(
           Alien(
-              'alien%d' % (n+1),
+              n,
               (
                   ALIEN_ROW_LEFT + ALIEN_H_SPACE * i,
                   ALIEN_TOP + ALIEN_V_SPACE * n),
               ALIEN_ROW_LEFT + ALIEN_H_SPACE * i,
               ALIEN_ROW_RIGHT - ALIEN_H_SPACE * (ALIEN_ROW_LENGTH - (i + 1))))
 
+bullets = []
+
 def draw():
     screen.clear()
     ship.draw()
+
+    # Aliens
     for alien in aliens:
         alien.draw()
+
+    # Bullets
+    to_remove = []
+    for i, bullet in enumerate(bullets):
+        bullet.draw()
+        if bullet.y < 0:
+            to_remove.append(i)
+    for i in reversed(to_remove):
+        del bullets[i]
 
 def update():
     if keyboard.LEFT and ship.x > ship.x_min:
@@ -52,3 +80,9 @@ def update():
         ship.x += ship.speed
     for alien in aliens:
         alien.move()
+    for bullet in bullets:
+        bullet.move()
+
+def on_key_down(key):
+    if key == keys.SPACE and len(bullets) < MAX_BULLETS:
+        bullets.append(Bullet(-BULLET_SPEED, 'bullet', (ship.x, ship.y)))
