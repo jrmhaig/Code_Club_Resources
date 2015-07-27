c1fdb75b2a5be4203f20b6d203b4ed6fbe99ceba
Joseph Haig <josephhaig@gmail.com>
2015-07-27 04:16:10 -0700

    A row of aliens
    Not quite moving properly yet

--- SpaceInvaders/space_invaders.py
+++ SpaceInvaders/space_invaders.py
@@ -18,16 +18,29 @@ class Alien(Actor):
         if self.x >= self.x_max or self.x <= self.x_min:
             self.speed = -self.speed
 
-alien1 = Alien('alien1', (100, 100), 100, 500)
+ALIEN_ROW_LENGTH = 10
+ALIEN_TOP = ( 5 * HEIGHT ) / 100
+ALIEN_ROW_LEFT = (5 * WIDTH) / 100
+ALIEN_ROW_RIGHT = (95 * WIDTH) / 100
+ALIEN_SPACE = ( 7 * WIDTH ) / 100
+aliens = []
+for i in range(ALIEN_ROW_LENGTH):
+    aliens.append(Alien('alien1',
+        ( ALIEN_ROW_LEFT + ALIEN_SPACE * i, ALIEN_TOP ),
+        ALIEN_ROW_LEFT, ALIEN_ROW_RIGHT))
+
+#alien1 = Alien('alien1', (100, 100), 100, 500)
 
 def draw():
     screen.clear()
     ship.draw()
-    alien1.draw()
+    for alien in aliens:
+        alien.draw()
 
 def update():
     if keyboard.LEFT and ship.x > ship.x_min:
         ship.x -= ship.speed
     if keyboard.RIGHT and ship.x < ship.x_max:
         ship.x += ship.speed
-    alien1.move()
+    for alien in aliens:
+        alien.move()
